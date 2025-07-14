61eaf71c1e2ea2561eddf13b46dcaac228aa2fe0
aniket jain <aniketjain2811@gmail.com>
2025-07-15 01:29:52 +0530

    Add vault.cpp and input.txt for file encryption project

--- vault.cpp
+++ vault.cpp
@@ -4,36 +4,49 @@
 
 using namespace std;
 
-void encryptDecrypt(string& data, char key) {
-    for (int i = 0; i < data.size(); i++) {
-        data[i] ^= key; // simple XOR encryption/decryption
+// Simple XOR encryption/decryption function
+void xorEncryptDecrypt(string &data, char key) {
+    for (size_t i = 0; i < data.size(); i++) {
+        data[i] ^= key;
     }
 }
 
 int main() {
-    string filename = "secret.txt";
-    string data;
+    string inputFile, encryptedFile = "encrypted.dat";
+    char key = 'K';
 
-    cout << "Enter data to encrypt: ";
-    getline(cin, data);
+    cout << "Enter input filename to encrypt: ";
+    getline(cin, inputFile);
 
-    encryptDecrypt(data, 'K'); // 'K' is the encryption key
+    ifstream fin(inputFile, ios::binary);
+    if (!fin) {
+        cout << "Error opening input file!" << endl;
+        return 1;
+    }
 
-    // Write encrypted data to file
-    ofstream outFile(filename, ios::binary);
-    outFile << data;
-    outFile.close();
+    // Read entire file content into a string
+    string fileData((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
+    fin.close();
 
-    cout << "Data encrypted and saved to " << filename << endl;
+    // Encrypt the file data
+    xorEncryptDecrypt(fileData, key);
 
-    // Now read and decrypt
-    ifstream inFile(filename, ios::binary);
-    string encryptedData((istreambuf_iterator<char>(inFile)), istreambuf_iterator<char>());
-    inFile.close();
+    // Write encrypted data to output file
+    ofstream fout(encryptedFile, ios::binary);
+    fout.write(fileData.c_str(), fileData.size());
+    fout.close();
 
-    encryptDecrypt(encryptedData, 'K'); // decrypt
+    cout << "File encrypted successfully into '" << encryptedFile << "'." << endl;
 
-    cout << "Decrypted data: " << encryptedData << endl;
+    // For verification, decrypt the file again and show preview
+    ifstream fin2(encryptedFile, ios::binary);
+    string encryptedData((istreambuf_iterator<char>(fin2)), istreambuf_iterator<char>());
+    fin2.close();
+
+    xorEncryptDecrypt(encryptedData, key);
+
+    cout << "Decrypted content preview (first 100 chars):" << endl;
+    cout << encryptedData.substr(0, 100) << endl;
 
     return 0;
 }
